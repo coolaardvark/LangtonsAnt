9263a23ab0758cd4147222e92ef0974ceb928b50
Mark Keightley <mark@keightley.net>
2020-08-01 13:54:37 +0100

    Add project files.

---
+++ LangtonsAnt/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace LangtonsAnt
+{
+    partial class FormAnt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelAntCanvas = new System.Windows.Forms.Panel();
+            this.labIterationCount = new System.Windows.Forms.Label();
+            this.btnStartStopAnt = new System.Windows.Forms.Button();
+            this.timerAnt = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panelAntCanvas
+            // 
+            this.panelAntCanvas.Location = new System.Drawing.Point(12, 144);
+            this.panelAntCanvas.Name = "panelAntCanvas";
+            this.panelAntCanvas.Size = new System.Drawing.Size(1595, 826);
+            this.panelAntCanvas.TabIndex = 0;
+            this.panelAntCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.panelAntCanvas_Paint);
+            // 
+            // labIterationCount
+            // 
+            this.labIterationCount.AutoSize = true;
+            this.labIterationCount.Location = new System.Drawing.Point(12, 54);
+            this.labIterationCount.Name = "labIterationCount";
+            this.labIterationCount.Size = new System.Drawing.Size(163, 32);
+            this.labIterationCount.TabIndex = 1;
+            this.labIterationCount.Text = "Iterations: 0";
+            // 
+            // btnStartStopAnt
+            // 
+            this.btnStartStopAnt.Location = new System.Drawing.Point(1360, 35);
+            this.btnStartStopAnt.Name = "btnStartStopAnt";
+            this.btnStartStopAnt.Size = new System.Drawing.Size(238, 69);
+            this.btnStartStopAnt.TabIndex = 2;
+            this.btnStartStopAnt.Text = "Start Ant";
+            this.btnStartStopAnt.UseVisualStyleBackColor = true;
+            this.btnStartStopAnt.Click += new System.EventHandler(this.btnStartStopAnt_Click);
+            // 
+            // timerAnt
+            // 
+            this.timerAnt.Interval = 250;
+            this.timerAnt.Tick += new System.EventHandler(this.timerAnt_Tick);
+            // 
+            // FormAnt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1619, 982);
+            this.Controls.Add(this.btnStartStopAnt);
+            this.Controls.Add(this.labIterationCount);
+            this.Controls.Add(this.panelAntCanvas);
+            this.Name = "FormAnt";
+            this.Text = "Langtons Ant";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelAntCanvas;
+        private System.Windows.Forms.Label labIterationCount;
+        private System.Windows.Forms.Button btnStartStopAnt;
+        private System.Windows.Forms.Timer timerAnt;
+    }
+}
+
---
+++ LangtonsAnt/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LangtonsAnt
+{
+    public partial class FormAnt : Form
+    {
+        int[,] antArray;
+        int antXPos;
+        int antYPos;
+
+        public FormAnt()
+        {
+            InitializeComponent();
+
+            initaliseAnt();
+        }
+
+        private void btnStartStopAnt_Click(object sender, EventArgs e)
+        {
+            if (timerAnt.Enabled == true)
+            {
+                timerAnt.Enabled = false;
+                btnStartStopAnt.Text = "Start Ant";
+            }
+            else
+            {
+                timerAnt.Enabled = true;
+                btnStartStopAnt.Text = "Stop Ant";
+            }
+        }
+
+        private void timerAnt_Tick(object sender, EventArgs e)
+        {
+            updateAnt();
+        }
+
+        private void panelAntCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics panelGrahpics = e.Graphics;
+        }
+
+        private void updateAnt()
+        {
+            panelAntCanvas.Update();
+        }
+
+        private void initaliseAnt()
+        {
+            // We should never called when the ant is running but to be sure
+            if (timerAnt.Enabled == true)
+            {
+                timerAnt.Enabled = false;
+            }
+
+            // Reset internal ant state
+            antArray = new int[99,99];
+            for (int x = 0; x < 100; x++)
+            {
+                for (int y = 0; y < 100; y++)
+                {
+                    antArray[x, y] = 0;
+                }
+            }
+
+            antXPos = 50;
+            antYPos = 50;
+
+            // Reset ant display
+        }
+    }
+}
